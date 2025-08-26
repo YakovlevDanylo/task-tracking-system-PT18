33a71cc9b1d9e92d97a2ab85c2530b6e157eaee3
YakovlevDanylo <yakovlevdv31@gmail.com>
2025-08-26 18:44:48 +0300

    CustomLoginForm

--- auth_system/forms.py
+++ auth_system/forms.py
@@ -1,8 +1,25 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username"]
 
+
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(attrs={
+            "class": "form-control",
+            "placeholder": "Enter your username"
+        })
+    )
+    password = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={
+            "class": "form-control",
+            "placeholder": "Enter your password"
+        })
+    )
