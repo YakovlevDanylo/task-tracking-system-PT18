7a69feab89a822feffd7fb4b7fe6da83c0d52222
YakovlevDanylo <yakovlevdv31@gmail.com>
2025-11-28 18:41:30 +0200

    Update form and create some tests

--- tasks/forms.py
+++ tasks/forms.py
@@ -7,7 +7,7 @@ class TaskForm(forms.ModelForm):
         fields = ["title", "description", "status", "priority", "end_time"]
 
     def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
+        super().__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs.update({"class": "form-control"})
 
--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,54 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse_lazy
+from django.utils import timezone
+
+from tasks.forms import TaskForm
+from tasks.models import Task
+
 
 # Create your tests here.
+class TaskFormTest(TestCase):
+
+    def test_form_valid(self):
+        form = TaskForm(data={"title": "New Title",
+                              "description": "New Description",
+                              "status": "todo",
+                              "priority": "low_priority",
+                              "end_time": timezone.now()
+                              })
+        self.assertTrue(form.is_valid())
+
+class TaskListViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_task_list_view_load(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+class TaskCreateViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username="testuser", password="12345")
+
+    def test_task_create_view_get(self):
+        self.client.force_login(self.user)
+
+        response = self.client.get("/create-task/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_task_create_view_post(self):
+        self.client.force_login(self.user)
+
+        response = self.client.post("/create-task/",
+                        {"title": "New Title",
+                              "description": "New Description",
+                              "status": "todo",
+                              "priority": "low_priority",
+                              })
+
+        self.assertEqual(Task.objects.count(), 1)
+
